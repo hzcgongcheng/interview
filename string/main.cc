fa976688e651d1947a30cfee200ef7c0a6222f6d
huzhicheng <hzcgongcheng@163.com>
2014-04-11 22:04:54 +0800

    second

--- string/main.cc
+++ string/main.cc
@@ -1,12 +1,38 @@
+#include "qh_string.h"
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "qh_string.h"
-
-
+#include <assert.h>
+using qh::string;
 int main(int argc, char* argv[])
 {
     //TODO 在这里添加单元测试，越多越好，代码路径覆盖率越全越好
     //TODO 单元测试写法请参考INIParser那个项目，不要写一堆printf，要用assert进行断言判断。
+	string  str1;	
+	string str2("hello world!");
+	int len = str2.size();
+	assert(len==12);//测试string::string( const char* s )构造函数
+	assert(str1.size()==0);//测试string::string(): data_(NULL), len_(0)构造函数
+	const char s='c';
+	string *str3 = new string(&s,10);
+	assert(str3->size()==10);//测试string::string( const char* s, size_t len )构造函数
+	delete str3;	
+	string str4(str2);
+	assert(str4.size()==str2.size());//测试string::string( const string& rhs )构造函数
+	str1 = str2;
+	assert(str1.size()==str2.size());//测试string& string::operator=( const string& rhs )操作函数
+	//assert(strcmp())
+	const char* data_copy = new char[str2.size()];
+	data_copy = str2.data();
+	assert(strcmp(data_copy,str2.data())==0);//测试 const char* string::data() const成员函数
+	assert(*str2[str2.size()-1]=='!');//测试char* string::operator[]( size_t index )操作函数
+	assert(data_copy[strlen(data_copy)-1]==*str2[str2.size()-1]);//测试data()返回字符数组最后为非NULL
+	assert(strcmp(str1.data(),str2.data())==0);
+	assert(strcmp(str4.data(),str2.data())==0);
+	const char* data_copy_str = str2.c_str();
+	assert(strcmp(data_copy_str,str2.data())==0);//测试const char* string::c_str() const成员函数
+	assert(data_copy_str[str2.size()]=='\0');//测试c_str()返回结果为以null终止的c字符串
+	printf("All test OK\n");
 
 #ifdef WIN32
     system("pause");
--- string/qh_string.cc
+++ string/qh_string.cc
@@ -9,48 +9,113 @@ namespace qh
     string::string()
         : data_(NULL), len_(0)
     {
+		this->data_ = NULL;
+		this->len_ = 0;
     }
 
     string::string( const char* s )
     {
+		if(!s) 
+		{
+			this->data_ = 0;
+			this->len_ = 0;		
+		}
+		else
+		{
+		    this->data_ = new char[strlen(s)+1];
+			strcpy(data_,s);
+			this->len_ = strlen(s);		
+		}
     }
 
     string::string( const char* s, size_t len )
     {
+		if(!len)
+		{
+			this->data_ = NULL;
+			this->len_ = 0;		
+		}
+		else
+		{
+			this->data_ = new char[len+1];
+			this->len_ = len;
+			for(int i=0;i<len;i++)
+				this->data_[i] = *s;		
+		}
     }
 
     string::string( const string& rhs )
-    {
+    {  
+	
+		if(!rhs.data_)
+		{
+			this->data_ = NULL;
+			this->len_ = 0;		
+		}
+		else
+		{
+			this->data_ = new char[rhs.size()+1];
+			strcpy(data_,rhs.data_);
+			this->len_ = strlen(rhs.data_);
+		}
 
     }
 
     string& string::operator=( const string& rhs )
     {
+		if(this != &rhs)
+		{
+			delete[] this->data_;
+			if(!rhs.data_)
+			{
+				this->data_ = 0;
+				this->len_ = 0;			
+			}
+			else
+			{
+				this->data_ = new char[rhs.size()+1];
+				strcpy(this->data_,rhs.data_);
+				this->len_ = rhs.len_;			
+			}
+		
+		}
         return *this;
     }
 
     string::~string()
     {
+		if(this->data_)
+		{
+			delete[] this->data_;
+			this->data_ = 0;
+			this->len_ = 0;
+		}
 
     }
 
     size_t string::size() const
     {
-        return len_;
+        return this->len_;
     }
 
     const char* string::data() const
     {
-        return NULL;
+		char* data_copy = new char[this->len_];
+		strcpy(data_copy,this->data_);
+        return data_copy;
     }
 
     const char* string::c_str() const
     {
-        return NULL;
+		char* data_copy = new char[this->len_+1];
+		strcpy(data_copy,this->data_);
+		data_copy[this->len_] = '\0';
+        return data_copy;
     }
 
     char* string::operator[]( size_t index )
     {
-        return NULL;
+		if(index>=0&&index<=this->len_-1)
+        return &this->data_[index];
     }
 }
